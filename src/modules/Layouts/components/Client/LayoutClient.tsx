8e787452e05a3408150af8482b1a5f70d0a22104
DESKTOP-1VIGCT6\Admin <nguyendinhtuananh7@gmail.com>
2025-07-31 08:20:41 +0700

    WIP: update LayoutClient before pulling

--- src/modules/Layouts/components/Client/LayoutClient.tsx
+++ src/modules/Layouts/components/Client/LayoutClient.tsx
@@ -2,8 +2,8 @@ import OverlaySearch from "@/component/Search/OverlaySearch";
 import { useState } from "react";
 import HeaderClient from "./HeaderClient";
 import FooterClient from "./FooterClient";
-import { Outlet } from "react-router-dom";
 import HomePage from "@/modules/HomePage/pages/HomePage";
+
 
 
 const UserLayout = () => {
